ac8a13df47233c6f2ad4781a29510101dde900f0
RYertayev <yertayevrk@gmail.com>
2024-05-08 21:30:36 +0500

    commit fps models

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -24,6 +24,7 @@ public class CharacterMovement : MonoBehaviour
         Movement();
         Jump();
     }
+   
     void Movement()
     {
         float horizontal = Input.GetAxis("Horizontal");
