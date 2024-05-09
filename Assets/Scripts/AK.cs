5b5b5f8c805e96ecfb0d9e9dc17b9a59c694979a
RYertayev <yertayevrk@gmail.com>
2024-05-09 20:04:38 +0500

    cartridges

--- Assets/Scripts/AK.cs
+++ Assets/Scripts/AK.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AK : MonoBehaviour
 {
     public float range = 200f;
     public Transform fpsCam;
     public ParticleSystem impact;
+    public AudioSource source;
+    public AudioClip shoot_sound;
+    private bool isShoot;
+    //Ammo
+    public int currentAmmo = 16;
+    public int spare_ammo = 16;
+    public int magazine = 52;
+    public Text ammoText;
+    public Text magazineText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isShoot = true;
+        ammoText.text = currentAmmo.ToString();
+        magazineText.text = magazine.ToString();
     }
 
     // Update is called once per frame
@@ -20,13 +31,37 @@ public class AK : MonoBehaviour
         {
             Shoot();
         }
+        if(currentAmmo == 0)
+        {
+          Reload();   
+        }
+        if(magazine == 4)
+        {
+            isShoot = false;
+        }
+       
+        
     }
     void Shoot()
     {
+        if(isShoot && currentAmmo > 0)
+        {
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Instantiate(impact, hit.point, Quaternion.identity);
         }
+        currentAmmo -= 1;
+        ammoText.text = currentAmmo.ToString();
+        source.PlayOneShot(shoot_sound);
+        }
+    }
+    void Reload()
+    {
+        magazine -= 16;
+        currentAmmo = spare_ammo;
+        ammoText.text = currentAmmo.ToString();
+        magazineText.text = magazine.ToString();
+        
     }
 }
